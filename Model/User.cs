1a47ee86a5abdcb820decd78ec823bd924e0040e
yuan teh chang <dnjsejr417@naver.com>
2023-07-22 23:32:15 +0900

    add Model

---
+++ Model/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookHub.Model
+{
+    public class User
+    {
+        private int id;
+        private int age;
+        private string name;
+        private string email;
+
+        public User()
+        {
+            //TODO mySQL에서 가져오는 코드
+        }
+        public int Id { get { return id; } }
+        public int Age { get { return age; } }
+        public string Name { get { return name; } }
+        public string Email { get { return email; } }
+    }
+}
